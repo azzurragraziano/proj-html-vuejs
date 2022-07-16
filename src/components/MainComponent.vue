2b457865d31222c93fd9afcaed6559c431ce12e6
azzurragraziano <azzurragraziano02@email.com>
2022-07-16 20:23:26 +0200

    first three section

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,6 +1,6 @@
 <template>
     <main>
-        <!-- our profucts -->
+        <!-- our products -->
         <section class="products">
             <div class="ms_container">
                 <div class="row">
@@ -35,6 +35,107 @@
                 </div>
             </div>
         </section>
+
+        <!-- corporate e weddings -->
+        <section class="corp-wed">
+            <div class="ms_container">
+
+                <span class="ms_categories">
+                    corporate e weddings
+                </span>
+
+                <h2 class="ms_font">
+                    Baking Special Moments
+                </h2>
+
+                <div class="row gx-5">
+                    <div class="col">
+                        <img src="../assets/img/wedding-bg.jpg" alt="corporate">
+                    </div>
+
+                    <div class="col">
+                        <img src="../assets/img/wedding-bg.jpg" alt="weddings">
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- some shopping -->
+        <section class="shopping">
+            <div class="ms_container">
+                <div class="row">
+                    <div class="col-4">
+                        <h2 class="ms_font">
+                            Find a freshly backed <br> product perfect for you
+                        </h2>
+
+                        <span class="ms_description">
+                            Corporis aut ut nulla natus reiciendis doloremque veritatis debitis odit dolorum ipsam dolores, quae vero, exercitationem dolorem.
+                        </span>
+
+                        <div>
+                            <a type="button" class="btn btn_purple">Shop All Pruducts</a>
+                        </div>
+                    </div>
+
+                    <div class="col-8">
+                        <div class="row">
+                            <div class="col">
+                                <!-- image -->
+                                <div class="card_image">
+                                    <img src="../assets/img/choco-chip-cookies.jpg" alt="Choco Chip Cookies">
+                                </div>
+
+                                <!-- text -->
+                                <div class="card_text">
+                                    <h3 class="ms_font">Choco Chip Cookies</h3>
+                                    <span>$19.00-$39.00</span>
+                                </div>
+                            </div>
+
+                            <div class="col">
+                                <!-- image -->
+                                <div class="card_image">
+                                    <img src="../assets/img/strawberry-jam-cookies.jpg" alt="Strawberry Jam Cookies">
+                                </div>
+
+                                <!-- text -->
+                                <div class="card_text">
+                                    <h3 class="ms_font">Strawberry Jam Cookies</h3>
+                                    <span>$24.00-$62.00</span>
+                                </div>
+                            </div>
+
+                            <div class="col">
+                                <!-- image -->
+                                <div class="card_image">
+                                    <img src="../assets/img/strawberry-donut.jpg" alt="Strawberry Donut">
+                                </div>
+
+                                <!-- text -->
+                                <div class="card_text">
+                                    <h3 class="ms_font">Strawberry Donut</h3>
+                                    <span>$24.00-$42.00</span>
+                                </div>
+                            </div>
+
+                            <div class="col">
+                                <!-- image -->
+                                <div class="card_image">
+                                    <img src="../assets/img/perfect-macarons.jpg" alt="Perfect Macarons">
+                                </div>
+
+                                <!-- text -->
+                                <div class="card_text">
+                                    <h3 class="ms_font">Perfect Macarons</h3>
+                                    <span>$18.00-$52.00</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </main>
 </template>
 
@@ -48,14 +149,50 @@ export default {
 @import '@/style/common';
 @import '@/style/variables';
 
-    .products {
-        margin-block: 9.4rem;
+    // sfrutto il margin collapse per non scrivere troppo css ripetuto 
+    section {
+        margin-block: 9.4rem;    
+    }
 
+    .products {
         .ms_font {
-            color: $primary-font-color;
-            font-size: 1.6rem;
-            font-weight: 900;
             margin-block: 1rem 3rem;
-        }        
+        }
+
+    }
+    .corp-wed {
+        text-align: center;
+    }
+
+    .shopping {
+        .col-4 {
+            text-align: center;
+            margin-top: 3rem;
+
+            h2 {
+                margin-block: 1rem;
+            }
+
+            div 
+            {
+                margin-block: 1rem;
+            }
+        }
+
+        .col-8 {
+            .card_text {
+                text-align: center;
+
+                .ms_font {
+                    font-size: 1rem;
+                    margin-block: 1rem;
+                }
+
+                span {
+                    font-size: 0.8rem;
+                    color: $primary-font-color;
+                }
+            }
+        }
     }
 </style>
