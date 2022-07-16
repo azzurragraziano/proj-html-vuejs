f3b2477492a102994ca65c00b2ba1cc032d2ce14
azzurragraziano <azzurragraziano02@email.com>
2022-07-16 16:45:26 +0200

    first section done

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,21 @@
     <!-- header -->
     <HeaderComponent />
 
+    <!-- main -->
+    <MainComponent />
   </div>
 </template>
 
 <script>
 import HeaderComponent from './components/HeaderComponent.vue'
+import MainComponent from './components/MainComponent.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HeaderComponent
+    HeaderComponent,
+    MainComponent
   },
   
 }
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,61 @@
+<template>
+    <main>
+        <!-- our profucts -->
+        <section class="products">
+            <div class="ms_container">
+                <div class="row">
+
+                    <!-- col with text -->
+                    <div class="col-4">
+                        
+                        <span class="ms_categories">
+                            our products
+                        </span>
+
+                        <p class="ms_font">All our delectable pastries are backed fresh in our Kitchen every morning, and are made with all-natural, all organic ingredients.</p>
+                    
+                        <span>
+                            <a type="button" class="btn btn_purple">Start Shopping</a>
+                        </span>
+                        
+                    </div>
+
+                    <!-- col with images -->
+                    <div class="col-8">
+                        <div class="row">
+                            <div class="col">
+                                <img src="../assets/img/choco-chip-cookies.jpg" alt="choco chip cookies">
+                            </div>
+
+                            <div class="col">
+                                <img src="../assets/img/strawberry-jam-cookies.jpg" alt="choco chip cookies">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+</template>
+
+<script>
+export default {
+   
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/style/common';
+@import '@/style/variables';
+
+    .products {
+        margin-block: 9.4rem;
+
+        .ms_font {
+            color: $primary-font-color;
+            font-size: 1.6rem;
+            font-weight: 900;
+            margin-block: 1rem 3rem;
+        }        
+    }
+</style>
